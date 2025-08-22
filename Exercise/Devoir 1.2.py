a11eb491a1fcde4c6f97336ddb956aa22e0ea0a7
gbertand <gb@>
2025-08-22 15:44:59 -0400

    V5

--- Exercise/Devoir 1.2.py
+++ Exercise/Devoir 1.2.py
@@ -25,6 +25,12 @@ class Compte:
             raise ValueError("Montant non valide")
         if montant < 0:
             raise ValueError("Montant négatif")
+        self.__solde_compte = montant
+    @nbre_operations.setter
+    def nbre_operations(self, nb_operations):
+        if nb_operations < 0:
+            raise ValueError("Nombre d'opération négative")
+        self.__nbre_operations = nb_operations
 
 
     def deposer(self, montant: float):
@@ -44,7 +50,7 @@ class Compte:
         return True
 
     def __repr__(self):
-        return f"Numéro:{self.__numero_compte}, Titulaire:{self.__titulaire_compte}, Solde{self.__solde_compte}, Nombre opération{self.__nbre_operations} "
+        return f"Numéro:{self.__numero_compte}, Titulaire:{self.__titulaire_compte}, Solde: {self.__solde_compte}, Nombre opération: {self.__nbre_operations} "
 
 
 class CompteCourant(Compte):
@@ -56,7 +62,7 @@ class CompteCourant(Compte):
         return self.__frais_retrait
     @frais_retrait.setter
     def frais_retrait(self, frais):
-        if self.__frais_retrait >= 0:
+        if frais <= 0:
             raise ValueError("Les frais de retrais doivent être positif")
         self.__frais_retrait = frais
     #TODO Redéfinit retirer(montant) : vérifie et débite montant + frais_retrait(refus si dépasse le solde).
@@ -64,10 +70,10 @@ class CompteCourant(Compte):
     def retirer(self, montant: float): #TODO quesce que sa veut dire de débite le montant
         if montant <= 0:
             raise ValueError("Refus!!\nMontant invalide (négatif)")
-        if montant + self.__frais_retrait > self.__solde_compte:
+        if montant + self.__frais_retrait > self.solde_compte:
             raise ValueError("Refus!!\nVous n'avez pas assez de fond")
-        self.__solde_compte -= (montant + self.__frais_retrait)
-        self.__nbre_operations += 1
+        self.solde_compte -= (montant + self.__frais_retrait)
+        self.nbre_operations += 1
         return True
 
 
@@ -89,9 +95,9 @@ class CompteEpargne(Compte):
         self.__taux = taux
 
     def capitaliser(self):
-        if 0 >= self.__solde_compte:
+        if 0 >= self.solde_compte:
             raise ValueError("Votre solde est nul")
-        self.__solde_compte += (self.__taux * self.__solde_compte)
+        self.solde_compte += (self.__taux * self.solde_compte)
         return True
 
 
@@ -122,13 +128,13 @@ class CompteRemunere(CompteEpargne):
         self.__bonus = bonus
 
     def capitaliser(self):
-        if 0 >= self.__solde_compte:
+        if 0 >= self.solde_compte:
             raise ValueError("Votre solde est nul")
-        if self.__solde_compte >= self.__palier:
-            self.__solde_compte += self.__solde_compte * ((1 + self.__bonus) * (1 + self.__taux) - 1)
+        if self.solde_compte >= self.__palier:
+            self.solde_compte += self.solde_compte * ((1 + self.__bonus) * (1 + self.taux) - 1)
             return True
         else:
-            self.__solde_compte += self.__solde_compte * self.__taux
+            self.solde_compte += self.solde_compte * self.taux
             return False
 
 class CompteCourantPlafonne(CompteCourant):
@@ -138,7 +144,7 @@ class CompteCourantPlafonne(CompteCourant):
         self.__reste = reste
         self.__hist = []
 
-            # TODO ou doit je mettre les condition si je veux seulement une lecture et non écriture
+ # TODO ou doit je mettre les condition si je veux seulement une lecture et non écriture
 
     @property
     def plafond(self):
@@ -156,6 +162,11 @@ class CompteCourantPlafonne(CompteCourant):
             raise ValueError("Le plafond doit être positif.")
         self.__plafond = plafond
         self.__reste = plafond
+    @hist.setter
+    def hist(self, hist):
+        if not isinstance(hist,str):
+            raise ValueError("Doit être mots")
+        self.__hist = hist
 
     def reset_plafond(self):  # TODO je ne comprend pas sa sert a quoi
         self.__plafond = self.__reste
@@ -163,21 +174,41 @@ class CompteCourantPlafonne(CompteCourant):
     def deposer(self, montant):
         succes = super().deposer(montant)
         if succes:
-            self.hist.append("DEPOT_OK")
+            self.__hist.append("DEPOT_OK")
 
     def retirer(self, montant):
         if self.__reste <= montant:
-            self.hist.append("REFUS_PLAFOND")
+            self.__hist.append("REFUS_PLAFOND")
             return False
-        if super().retirer(montant):
+        if super().retirer(montant): # TODO pas bon mais sais pas pourquoi
+            self.solde_compte -= montant
             montant -= self.__reste
-            self.hist.append("RETRAIT_OK")
+            self.__hist.append("RETRAIT_OK")
             return True
-        self.hist.append("REFUS_SOLDE")
+        self.__hist.append("REFUS_SOLDE")
         return False
 
 compte = CompteCourant("12345", "Gabriel Bertrand", 0, 0, 1.0)
 compte.deposer(100)
 compte.retirer(20)
-# compte.retirer(100)
+print(compte.solde_compte) # Me donne 79.
+#compte.retirer(100)
+
+compte2 = CompteEpargne("1234", "Gabriel Bertrand", 0, 0, 0.05)
+compte2.deposer(200)
+compte2.capitaliser()
+print(compte2.solde_compte) # Me donne 210.
+
+compte3 = CompteRemunere("1234", "Gabriel Bertrand", 0, 0, 0.05, 1000, 0.10)
+compte3.deposer(1000)
+compte3.capitaliser()
+print(compte3.solde_compte) # Me donne 1155.
+
+compte4 = CompteCourantPlafonne("1234", "Gabriel Bertrand", 0, 0, 2, 300, 0)
+compte4.deposer(500)
+compte4.retirer(200)
+compte4.retirer(150)
+compte4.reset_plafond()
+compte4.retirer(150)
+print(compte4.hist)
 
