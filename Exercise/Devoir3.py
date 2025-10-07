a06911ab29ce36d8b3970ccfa02ac88c55b61eb0
gbertand <gb@>
2025-10-07 19:58:06 -0400

    V16

--- Exercise/Devoir3.py
+++ Exercise/Devoir3.py
@@ -30,7 +30,7 @@ class Compte(tk.Tk):
         self.label3.grid(row=1, column=0, sticky="e")
         self.label4 = ttk.Label(self.frame1, text="Solde:", font=("Arial", 10))
         self.label4.grid(row=2, column=0, sticky="e")
-        self.entry1 = ttk.Entry(self.frame1, font=("Arial", 10))
+        self.entry1 = ttk.Entry(self.frame1,font=("Arial", 10))
         self.entry1.grid(row=0, column=1, sticky="ew", padx=10)
         self.gele = tk.BooleanVar(value=False)
         self.checkbutton1 = ttk.Checkbutton(self.frame1, text="Gelé", variable=self.gele, command=self.geler)
@@ -49,7 +49,8 @@ class Compte(tk.Tk):
         self.frame2.columnconfigure(1, weight=1)
         self.frame2.rowconfigure(1, weight=1)
         self.frame2.rowconfigure(2, weight=1)
-        self.entry4 = ttk.Entry(self.frame2, font=("Arial", 10))
+        self.entry_montant = tk.StringVar()
+        self.entry4 = ttk.Entry(self.frame2, textvariable=self.entry_montant, state="readonly", font=("Arial", 10))
         self.entry4.grid(row=0, column=0)
         self.bt1 = ttk.Button(self.frame2, text= "Random", command=self.random,state="active")
         self.bt1.grid(row=1, column=0, padx=10)
@@ -83,19 +84,30 @@ class Compte(tk.Tk):
 
     # Fonction pour gén.rer nombre au hazard
     def random(self):
-        self.entry4.delete(0, "end")
+        if self.valeurs.get() == "":
+            messagebox.showwarning("Génération de montant impossible", "Veuillez sélectionner un des radio bouton")
+            return
+        self.entry_montant.set("")
         intervalle = self.valeurs.get()
         liste = list(map(int, intervalle.split()))
         valeur = str(round(random.uniform(liste[0], liste[1]),2))
-        self.entry4.insert("end",valeur)
+        self.entry_montant.set(valeur)
+    def condition(self):
+        if len(self.entry1.get().strip()) != 5 or not self.entry1.get().isdigit():
+            messagebox.showwarning("Dépot impossible", "Veuillez entrer un numéro valide d'exactement 5 chiffres.")
+            return True
+        if len(self.entry2.get().strip()) < 5 or not self.entry2.get().strip().replace(" ", "").isalpha():
+            messagebox.showwarning("Dépot impossible", "Veuillez entrer un nom de détenteur valide d'au moins 5 caractères.")
+            return True
+        if self.entry4.get() == "":
+            messagebox.showwarning("Dépot impossible", "Veuillez faire générer un montant random")
+            return True
 
     def deposer(self):
         clé = (self.entry1.get(), self.entry2.get().strip())
         num1, det1 = clé
-        if len(self.entry1.get().strip()) != 5 :
-            messagebox.showwarning("Dépot impossible", "Veuillez entrer un numéro valide d'exactement 5 chiffres.")
-        if len(self.entry2.get().strip()) < 5 :
-            messagebox.showwarning("Dépot impossible", "Veuillez entrer un nom de détenteur valide d'au moins 5 caractères.")
+        if self.condition():
+            return
         for clés in self.utilisateur:
             num2, det2 = clés
             if num1 == num2 and det1 != det2:
@@ -109,25 +121,33 @@ class Compte(tk.Tk):
             self.entry3.insert("end", str(round(depot + montant_initial, 2)))
             self.entry3.config(state="readonly")
             self.utilisateur[clé] += depot
-            print(1)
+
         if not clé in self.utilisateur:
             depot = float(self.entry4.get())
             self.entry3.config(state="normal")
+            self.entry3.delete(0, "end")
             self.entry3.insert(0, str(round(depot, 2)))
             self.entry3.config(state="readonly")
             self.utilisateur[self.entry1.get(), self.entry2.get().strip()] = depot
 
     def retirer(self):
-        if len(self.entry1.get().strip()) != 5:
-            messagebox.showwarning("Dépot impossible", "Veuillez entrer un numéro valide d'exactement 5 chiffres.")
-        if len(self.entry2.get().strip()) < 5:
-            messagebox.showwarning("Dépot impossible",
-                                   "Veuillez entrer un nom de détenteur valide d'au moins 5 caractères.")
-        else:
+        clé = (self.entry1.get(), self.entry2.get().strip())
+        if self.condition():
+            return
+        if not clé in self.utilisateur:
+            messagebox.showwarning("Compte introuvable", "Veuillez vous créé un compte en déposant un montant")
+            return
+        if clé in self.utilisateur:
             retrait = float(self.entry4.get())
-            montant_initial = float(self.entry3.get())
+            montant_initial = self.utilisateur[clé]
+            if (montant_initial - retrait) < 0:
+                messagebox.showwarning("Fonds insufisant", "Vous n'avez pas assez dans votre compte pour retirer ce montant")
+                return
+            self.entry3.config(state="normal")
             self.entry3.delete(0, "end")
             self.entry3.insert("end", str(round(montant_initial - retrait, 2)))
+            self.entry3.config(state="readonly")
+            self.utilisateur[clé] -= retrait
 
     def reset(self):
         self.entry1.delete(0, "end")
@@ -135,11 +155,17 @@ class Compte(tk.Tk):
         self.entry3.config(state= "normal")
         self.entry3.delete(0, "end")
         self.entry3.config(state="readonly")
-        self.entry4.delete(0, "end")
-        self.valeurs = tk.StringVar(value="")
+        self.entry_montant.set("")
+        self.valeurs.set("")
+        self.gele.set(False)
 
     def vider(self):
+        clé = (self.entry1.get(), self.entry2.get().strip())
+        self.utilisateur[clé] = 0
+        self.entry3.config(state="normal")
         self.entry3.delete(0, "end")
+        self.entry3.insert(0,"0")
+        self.entry3.config(state="readonly")
 
     def geler(self):
         if self.gele.get():
