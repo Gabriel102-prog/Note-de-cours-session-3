d09b1bc3a34bcc28279b50d658c0ddcefe18ca16
6291581 <fghbdr@rrh>
2025-10-07 17:50:57 -0400

    V18

--- Exercise/Cours7.2.py
+++ Exercise/Cours7.2.py
@@ -1,10 +1,11 @@
 import tkinter as tk
-from tkinter import ttk as ttk
-
-
+from tkinter import ttk as ttk, messagebox
+import json
+from tkinter import filedialog
 class Formulaire(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.row = []
         self.title("Formulaire")
         self.geometry("800x600")
         self.frame = ttk.Frame(self)
@@ -34,16 +35,16 @@ class Formulaire(tk.Tk):
         self.entry2.grid(row=1, column=1, sticky="ew", padx=10)
         self.entry3 = ttk.Entry(self.frame)
         self.entry3.grid(row=1, column=2, sticky="ew", padx=10)
-        self.bout1 = ttk.Button(self.frame, text="Ajouter")
+        self.bout1 = ttk.Button(self.frame, text="Ajouter", command=self.ajouter)
         self.bout1.grid(row=1, column=3, sticky="ew", padx=10)
-        self.bout2 = ttk.Button(self.frame, text="Supprimer sélection")
+        self.bout2 = ttk.Button(self.frame, text="Supprimer sélection", command=self.supprimer)
         self.bout2.grid(row=1, column=4, sticky="ew", padx=10)
         # Frame2
         self.frame2 = ttk.Frame(self)
         self.frame2.grid(row=1, column=0, sticky="nsew")
         self.frame2.columnconfigure(0, weight=1)
         self.frame2.rowconfigure(0, weight=1)
-        self.list = ttk.Treeview(self.frame2, columns=("Nom", "Email", "Âge"), show="headings")
+        self.list = ttk.Treeview(self.frame2, columns=("Nom", "Email", "Âge"), show="headings", selectmode="extended")
         self.list.heading("Nom", text="Nom")
         self.list.heading("Email", text="Email")
         self.list.heading("Âge", text="Âge")
@@ -61,14 +62,79 @@ class Formulaire(tk.Tk):
         self.frame3.columnconfigure(2, weight=10)
         self.frame3.columnconfigure(3, weight=1)
         self.frame3.columnconfigure(4, weight=1)
-        self.bout3 = ttk.Button(self.frame3, text="Import JSON")
+        self.bout3 = ttk.Button(self.frame3, text="Import JSON", command=self.importer_JSON)
         self.bout3.grid(row=0, column=0, sticky="ew" )
         self.bout4 = ttk.Button(self.frame3, text="Import CSV")
         self.bout4.grid(row=0, column=1, sticky="ew")
-        self.bout5 = ttk.Button(self.frame3, text="Import Json")
+        self.bout5 = ttk.Button(self.frame3, text="Exporter Json", command=self.exporter_JSON)
         self.bout5.grid(row=0, column=3, sticky="ew")
         self.bout6 = ttk.Button(self.frame3, text="Exporter CSV")
         self.bout6.grid(row=0, column=4, sticky="ew")
+
+    def ajouter(self):
+        nom = self.entry1.get()
+        email = self.entry2.get()
+        age = self.entry3.get()
+        self.list.insert("","end", values=(nom, email, age))
+        self.entry1.delete(0, "end")
+        self.entry2.delete(0, "end")
+        self.entry3.delete(0, "end")
+        self.row.append([{"nom": nom, "email": email, "age": age}])
+    def supprimer(self):
+        selection = self.list.selection()
+        for item in selection:
+            if item == selection[0]:
+                self.list.delete(item)
+
+    def exporter_JSON(self):
+        if not self.row:
+            messagebox.showerror("Export json", "Le fichier n'existe pas")
+            return
+        path = filedialog.asksaveasfilename(title= "exporterjson",defaultextension=".json", filetypes=(("JSON Files", "*.json"),))
+        if not path:
+            return
+        try:
+            with open(path, "w", encoding="utf-8") as f:
+                json.dump(self.row, f, indent=4)
+            messagebox.showinfo("Exporter json", "Exporter json exported successfully")
+        except Exception as e:
+            messagebox.showerror("Exporter json", str(e))
+
+    def importer_JSON(self):
+        path = filedialog.askopenfilename(title= "importerjson",filetypes=(("JSON Files", "*.json"),))
+        if not path:
+            return
+        try:
+            with open(path, "r", encoding="utf-8") as f:
+                data = json.loads(f)
+            if not isinstance(data, list):
+                raise ValueError("n'est pas une list")
+            self.row = []
+            for r in data:
+                if isinstance(r, dict):
+                    ligne = {}
+                    for k in FIELDS:
+                        ligne[k] = r.get(k, "")
+                    self.row.append(ligne)
+            self.refresh()
+            messagebox.showinfo("Importer json", "Importer json exported successfully")
+        except Exception as e:
+            messagebox.showerror("Importer json", str(e))
+
+    def refresh(self):
+        enfants = self.list.get_children()
+        for e in enfants:
+            self.list.delete(e)
+
+        for r in self.row:
+            nom = r.get("nom", "")
+            email = r.get("email", "")
+            age = r.get("age", "")
+
+            self.list.insert("", "end", values=(nom, email, age))
+
+
+
 
 
 if __name__ == "__main__":
